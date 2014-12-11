3b51b5a7dfee214fcdee132e1ebe33d0b00fbb6d
darkkaos505 <callum.waddell@btinternet.com>
2014-12-11 22:38:54 +0000

    Started Base Sprite Class
    Create Base class for base sprite which will be used for ships and for
    station modules

---
+++ LudamDare31/Assets/Scripts/SpriteAnimCon.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+    [RequireComponent(typeof(Animator))]
+    [RequireComponent(typeof(Rigidbody2D))]
+
+public class SpriteAnimCon : MonoBehaviour 
+{
+    Animator anim;
+    
+
+	// Use this for initialization
+	void Start () 
+    {
+        anim = GetComponent<Animator>();	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+
+        anim.SetFloat("Speed",  rigidbody2D.velocity.magnitude);
+	}
+
+  public  float CallDeathAnimation()
+    {
+        float length = 1;
+
+        anim.SetTrigger("Death");
+
+        length = anim.GetCurrentAnimationClipState(0).Length;
+
+        return length;
+    }
+
+}
+
---
+++ LudamDare31/Assets/Scripts/SpriteBase.cs
@@ -0,0 +1,88 @@
+﻿using UnityEngine;
+using System.Collections;
+
+ [RequireComponent(typeof(Rigidbody2D))]
+ [RequireComponent(typeof(SpriteAnimCon))]
+
+public class SpriteBase : MonoBehaviour 
+{
+	const float maxHealth = 100;
+	float health = maxHealth;
+
+    float force = 15;
+    float rotateSpeed = 20;
+
+    public GameObject BarPrefab = null;
+
+
+    public GameObject hpTransform;
+
+
+    SpriteAnimCon spriteAnimCon;
+
+    BarScript hpBarScript;
+ 	// Use this for initialization
+	void Start ()
+    {
+        spriteAnimCon  = GetComponent<SpriteAnimCon>();
+
+        if (BarPrefab != null)
+        {
+            HitPointBar();
+        }
+	}
+
+     void HitPointBar()
+    {
+        GameObject hpBarObject = Instantiate(BarPrefab, hpTransform.transform.position, Quaternion.identity) as GameObject;
+        hpBarScript = hpBarObject.GetComponentInChildren<BarScript>();
+        hpBarScript.objectToFollow = hpTransform.transform;
+    }
+
+	
+	// Update is called once per frame
+	void Update () 
+    {
+        TestControls();
+
+	}
+
+     void TestControls()
+     {
+
+        if(Input.GetKey(KeyCode.W))
+        {
+
+            transform.rigidbody2D.AddForce(transform.up * Time.deltaTime * force);
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(transform.forward, rotateSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-transform.forward, rotateSpeed * Time.deltaTime, Space.Self);
+        }
+
+        if (Input.GetKey(KeyCode.Alpha1))
+        {
+            StartDeath();
+        }
+      
+     }
+
+
+    public void StartDeath()
+     {
+         float length = spriteAnimCon.CallDeathAnimation();
+         Invoke("EndDeath", length);
+     }
+
+     void EndDeath()
+     {
+         Destroy(gameObject);
+     }
+
+}
